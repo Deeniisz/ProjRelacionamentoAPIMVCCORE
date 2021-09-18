f98b93cff662915e7aa29f7c33b8b10a47a4728c
Deeniisz <assis.denis98@gmail.com>
2021-09-18 09:44:26 -0300

    Ok

---
+++ ProjRelacionamentoMVCAPINETCORE/Data/ProjRelacionamentoMVCAPINETCOREContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjRelacionamentoMVCAPINETCORE.Models;
+
+namespace ProjRelacionamentoMVCAPINETCORE.Data
+{
+    public class ProjRelacionamentoMVCAPINETCOREContext : DbContext
+    {
+        public ProjRelacionamentoMVCAPINETCOREContext (DbContextOptions<ProjRelacionamentoMVCAPINETCOREContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjRelacionamentoMVCAPINETCORE.Models.Carro> Carro { get; set; }
+
+        public DbSet<ProjRelacionamentoMVCAPINETCORE.Models.Locacao> Locacao { get; set; }
+    }
+}
